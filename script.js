9fee4b21f604293a26b09d740e2529c944b6bbe7
Valeriia Isaienko <93624578+Magic1vy@users.noreply.github.com>
2023-05-10 18:35:14 -0400

    script.js

--- script.js
+++ script.js
@@ -1,63 +1,20 @@
-const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+let lastChecked
 
-let lastChecked;
-let startX;
-let endX;
+function handleCheck(e){
 
-function handleTouchStart(e) {
-  startX = e.touches[0].pageX;
+    let inBetween = false
+    if (e.shiftKey && this.checked){
+        checkboxes.forEach( checkbox =>{
+            if (checkbox === this || checkbox === lastChecked){
+                inBetween = !inBetween  ;
+            }
+            if (inBetween){
+                checkbox.checked = true; 
+            }
+        })
+    }
+
+    lastChecked = this;
 }
 
-function handleTouchMove(e) {
-  endX = e.touches[0].pageX;
-}
-
-function handleTouchEnd(e) {
-  if (endX < startX) {
-
-    checkboxes.forEach((checkbox) => {
-      if (
-        (checkbox.offsetLeft > lastChecked.offsetLeft &&
-          checkbox.offsetLeft <= endX) ||
-        (checkbox.offsetLeft < lastChecked.offsetLeft &&
-          checkbox.offsetLeft >= endX)
-      ) {
-        checkbox.checked = true;
-      }
-    });
-  } else if (endX > startX) {
-
-    checkboxes.forEach((checkbox) => {
-      if (
-        (checkbox.offsetLeft < lastChecked.offsetLeft &&
-          checkbox.offsetLeft >= endX) ||
-        (checkbox.offsetLeft > lastChecked.offsetLeft &&
-          checkbox.offsetLeft <= endX)
-      ) {
-        checkbox.checked = true;
-      }
-    });
-  }
-}
-
-function handleCheck(e) {
-  let inBetween = false;
-  if (e.shiftKey && this.checked) {
-    checkboxes.forEach((checkbox) => {
-      if (checkbox === this || checkbox === lastChecked) {
-        inBetween = !inBetween;
-      }
-      if (inBetween) {
-        checkbox.checked = true;
-      }
-    });
-  }
-  lastChecked = this;
-}
-
-checkboxes.forEach((checkbox) => {
-  checkbox.addEventListener('click', handleCheck);
-  checkbox.addEventListener('touchstart', handleTouchStart);
-  checkbox.addEventListener('touchmove', handleTouchMove);
-  checkbox.addEventListener('touchend', handleTouchEnd);
-});
+checkboxes.forEach( checkbox => checkbox.addEventListener("click", handleCheck));
